4b3ab5f7c93166232fd6b6aeff8a300925e945a4
Nanthakumar <nanthanaveen565@gmail.com>
2025-08-21 21:04:09 +0530

    [Day2]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+// import { NgForOf } from "../../node_modules/@angular/common/index";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule,RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,4 +23,27 @@ export class AppComponent {
 
   isdisabled: boolean = false;
   isactive: boolean = true;
+
+  //two-way data binding
+
+  name1: string = "Sudhirkumar";
+  name2:string="Sudhir";
+
+  ngif: boolean = true;
+  islogin: boolean = true; 
+  islogin1: boolean = false;
+
+
+
+ //ng For
+  city:string[]=['chennai','banglore','mysore'];
+
+  //for
+  std:any[]=[
+    {
+      stdId:1,stdname:"Geet",stdcity:"chennai"
+    }
+  ]
+ 
 }
+
